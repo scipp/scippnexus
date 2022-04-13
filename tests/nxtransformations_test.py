10906e4ef3da0805cc07fa787f4527604e151823
Simon Heybrock <simon.heybrock@ess.eu>
2022-04-13 10:21:52 +0200

    Test transformation chain with time dependence

--- tests/nxtransformations_test.py
+++ tests/nxtransformations_test.py
@@ -108,3 +108,36 @@ def test_Transformation_with_multiple_values(nxroot):
     assert sc.identical(t.offset, offset)
     assert sc.identical(t.vector, vector)
     assert sc.identical(t[()], expected)
+
+
+def test_chain_with_multiple_values(nxroot):
+    detector = create_detector(nxroot)
+    detector.create_field('depends_on', sc.scalar('/detector_0/transformations/t1'))
+    transformations = detector.create_class('transformations',
+                                            NX_class.NXtransformations)
+    log = sc.DataArray(
+        sc.array(dims=['time'], values=[1.1, 2.2], unit='m'),
+        coords={'time': sc.array(dims=['time'], values=[11, 22], unit='s')})
+    log.coords['time'] = sc.epoch(unit='ns') + log.coords['time'].to(unit='ns')
+    offset = sc.spatial.translation(value=[1, 2, 3], unit='m')
+    vector = sc.vector(value=[0, 0, 1])
+    t = log * vector
+    t.data = sc.spatial.translations(dims=t.dims, values=t.values, unit=t.unit)
+    value1 = transformations.create_class('t1', NX_class.NXlog)
+    value1['time'] = log.coords['time'] - sc.epoch(unit='ns')
+    value1['value'] = log.data
+    value1.attrs['depends_on'] = 't2'
+    value1.attrs['transformation_type'] = 'translation'
+    value1.attrs['offset'] = offset.values
+    value1.attrs['offset_units'] = str(offset.unit)
+    value1.attrs['vector'] = vector.value
+    value2 = transformations.create_class('t2', NX_class.NXlog)
+    value2['time'] = log.coords['time'] - sc.epoch(unit='ns')
+    value2['value'] = log.data
+    value2.attrs['depends_on'] = '.'
+    value2.attrs['transformation_type'] = 'translation'
+    value2.attrs['vector'] = vector.value
+
+    expected = sc.spatial.affine_transform(value=np.identity(4), unit=t.unit)
+    expected = t * (t * (offset * expected))
+    assert sc.identical(detector[...].coords['depends_on'].value, expected)
