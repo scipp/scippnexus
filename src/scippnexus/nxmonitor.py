5275cf49e96e8ccdbae589a163d64a092e6920e9
Simon Heybrock <simon.heybrock@ess.eu>
2022-04-25 11:17:56 +0200

    Comment

--- src/scippnexus/nxmonitor.py
+++ src/scippnexus/nxmonitor.py
@@ -9,6 +9,10 @@ from .nxdetector import NXdetector
 class NXmonitor(NXdetector):
     @property
     def _event_grouping(self) -> Dict[str, Union[str, Field]]:
+        # Unlike NXdetector, NXmonitor does not group by 'detector_number'. We pass
+        # grouping information does matches the underlying binning of NXevent_data
+        # such that no addition binning will need to be performed. That is, the by-pulse
+        # binning present in the file (in NXevent_data) is preserved.
         return {
             'groups_key': 'event_time_zero',
             'groups': self.events['event_time_zero']
