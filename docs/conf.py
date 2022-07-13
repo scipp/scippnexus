700e9b18f979355c0f8b1c4da3a78dcd7f895865
Simon Heybrock <simon.heybrock@ess.eu>
2022-07-13 14:02:46 +0200

    Fix link target

--- docs/conf.py
+++ docs/conf.py
@@ -174,8 +174,7 @@ html_theme_options = {
 if outdated:
     html_theme_options["announcement"] = (
         "⚠️ You are viewing the documentation for an old version of scippnexus. "
-        "Switch to <a href='https://github.com/scipp/scippnexus'>latest version.</a> ⚠️"
-    )
+        "Switch to <a href='https://scipp.github.io/scippnexus'>latest version.</a> ⚠️")
 
 html_logo = "_static/logo.png"
 html_favicon = "_static/favicon.ico"
