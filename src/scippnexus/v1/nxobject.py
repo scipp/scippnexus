1c3dbae97f28eb1e3c583e3ce6d39388b001ea1a
Mridul Seth <git@mriduls.com>
2024-04-23 13:39:58 +0200

    Use stdlib datetime to parse instead of dateutil

--- src/scippnexus/v1/nxobject.py
+++ src/scippnexus/v1/nxobject.py
@@ -10,7 +10,6 @@ import re
 import warnings
 from typing import Any, Callable, Dict, List, Optional, Protocol, Tuple, Union, overload
 
-import dateutil.parser
 import h5py
 import numpy as np
 import scipp as sc
@@ -107,8 +106,8 @@ def _as_datetime(obj: Any):
         try:
             # NumPy and scipp cannot handle timezone information. We therefore apply it,
             # i.e., convert to UTC.
-            # Would like to use dateutil directly, but with Python's datetime we do not
-            # get nanosecond precision. Therefore we combine numpy and dateutil parsing.
+            # Would like to use datetime directly, but with Python's datetime we do not
+            # get nanosecond precision. Therefore we combine numpy and datetime parsing.
             date_only = 'T' not in obj
             if date_only:
                 return sc.datetime(obj)
@@ -119,8 +118,8 @@ def _as_datetime(obj: Any):
                 # No timezone, parse directly (scipp based on numpy)
                 return sc.datetime(f'{date}T{time}')
             else:
-                # There is timezone info. Parse with dateutil.
-                dt = dateutil.parser.isoparse(obj)
+                # There is timezone info. Parse with datetime.
+                dt = datetime.datetime.fromisoformat(obj)
                 dt = dt.replace(microsecond=0)  # handled by numpy
                 dt = dt.astimezone(datetime.timezone.utc)
                 dt = dt.replace(tzinfo=None).isoformat()
