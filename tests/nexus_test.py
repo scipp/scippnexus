2556a0391886668eb422c4d44d6b3fb434223914
Johannes Kasimir <johanneskasimir@gmail.com>
2024-06-17 11:42:02 +0200

    fix: adapt to numpy 2.0

--- tests/nexus_test.py
+++ tests/nexus_test.py
@@ -93,8 +93,7 @@ def test_nxobject_entry(nxroot):
 
 def test_nx_class_can_be_bytes(h5root):
     log = h5root.create_group('log')
-    attr = np.chararray((), itemsize=5)
-    attr[()] = b'NXlog'
+    attr = np.array(b'NXlog', dtype='|S5')
     log.attrs['NX_class'] = attr
     group = snx.Group(log, definitions=snx.base_definitions())
     assert group.nx_class == NXlog
