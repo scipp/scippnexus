99e579ac4b485d996ecd9226837e5f00a81eee7f
George O'Neill <george.oneill@ess.eu>
2025-02-05 08:43:57 +0100

    updated test

--- tests/nxtransformations_test.py
+++ tests/nxtransformations_test.py
@@ -784,7 +784,8 @@ def test_compute_positions_handles_empty_time_dependent_transform_without_error(
 
     root = make_group(h5root)
     loaded = root[()]
-    with pytest.warns(UserWarning, match='depends_on chain contains empty time-series'):
+    warning_regex = r"depends_on chain .* contains empty time-series"
+    with pytest.warns(UserWarning, match=warning_regex):
         result = snx.compute_positions(loaded, store_transform='transform')
 
     # Even if only some of the logs are empty we cannot return any values.
