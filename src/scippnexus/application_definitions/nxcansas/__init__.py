fc44e21adb5018144913a0e47b9ad60b06d46c8b
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2025-06-04 10:34:56 +0200

    Update pre-commit hooks

--- src/scippnexus/application_definitions/nxcansas/__init__.py
+++ src/scippnexus/application_definitions/nxcansas/__init__.py
@@ -2,8 +2,7 @@
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 # @author Simon Heybrock
 
-# flake8: noqa
 
 from .nxcansas import SASdata, SASentry, definitions
 
-__all__ = ['definitions', 'SASentry', 'SASdata']
+__all__ = ['SASdata', 'SASentry', 'definitions']
